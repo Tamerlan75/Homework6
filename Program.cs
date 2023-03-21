cd9b13e6a72b242996eb2e047b8eef1ceadeb732
Timur <t.mollaev75@yandex.ru>
2023-03-21 23:09:02 +0300

    Homework6 not completed

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// int [] Array (int Size)
+// {
+//     int [] newArr = new int [Size];
+//   
+//     for ( int i = 0; i < Size; i++)
+//         newArr[i] = Convert.ToInt32(Console.ReadLine());
+//      return newArr;   
+// }        
+    
+    
+// void GreaterZero (int [] Arr)
+// {
+//  int nam = 0;
+//  for (int i = 0; i < Arr.Length; i++)
+//   {  
+//      if (Arr[i] > 0) 
+//          nam++;
+//   }      
+// Console.WriteLine($"{nam} Numbers greater then 0");
+// }
+// Console.WriteLine("Input M");
+// int Size  = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input M digits");
+// int[] Arr = Array( Size);
+// GreaterZero(Arr); 
+
+ 
